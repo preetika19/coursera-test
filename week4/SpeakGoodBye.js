9818700a53cec5b6405e44196e71bfa87c7551c3
Preetika Lakshmanan <preetika19@gmail.com>
2020-08-30 13:15:14 +0400

    Module4-solution

---
+++ week4/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+//SpeakGoodBye.js
+
+(function (window) {
+
+	var byeSpeaker = {};
+	byeSpeaker.speak = function(name){
+		console.log(speakWord + " " + name);
+	}
+	var speakWord = "Good Bye";
+
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ week4/SpeakHello.js
@@ -0,0 +1,13 @@
+//SpeakHello.js
+
+(function(window) {
+
+	var helloSpeaker = {};
+	helloSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	} 
+	var speakWord = "Hello";
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
